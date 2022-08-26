e8f94f6357f3022151126fc67988ba22c4388ceb
Vĩ Tăng <vi.tang@levincigroup.com>
2022-08-26 09:55:27 +0700

    Tutorial: Create a project

---
+++ microsoft.botsay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace microsoft.botsay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
